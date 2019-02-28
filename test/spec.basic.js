0f4c9736d418d3bc0305729ff7870756aed26536
Richardo2016 <richardo2016@gmail.com>
2019-02-28 18:06:16 +0800

    change.

---
+++ test/spec.basic.js
@@ -0,0 +1,41 @@
+const test = require('test')
+test.setup()
+
+describe('basic requirement', () => {
+    const react = require('react')
+
+    it('api exists', () => {
+        assert.isObject(react)
+
+        assert.isObject(react.Children)
+        assert.isFunction(react.createRef)
+        assert.isObject(react.Component)
+        assert.isObject(react.PureComponent)
+        assert.property(react, 'Fragment')
+
+        assert.isFunction(react.createContext)
+        assert.isFunction(react.createElement)
+        assert.isFunction(react.cloneElement)
+        assert.isFunction(react.createFactory)
+        assert.isFunction(react.isValidElement)
+        assert.isString(react.version)
+
+        if (react.version > "16.8.0") {
+            assert.isFunction(react.forwardRef)
+            assert.isFunction(react.useCallback)
+            assert.isFunction(react.useContext)
+            assert.isFunction(react.useDebugValue)
+            assert.isFunction(react.useEffect)
+            assert.isFunction(react.useImperativeHandle)
+            assert.isFunction(react.useLayoutEffect)
+            assert.isFunction(react.useMemo)
+            assert.isFunction(react.useReducer)
+            assert.isFunction(react.useRef)
+            assert.isFunction(react.useState)
+        }
+    })
+})
+
+if (require.main === module) {
+    test.run(console.DEBUG)
+}
